0b49495a15c00106afc6f5af474ca3eaf23b357a
ac2393921 <ac2393921@gmail.com>
2023-06-04 21:41:23 +0900

    add: 求人作成機能を追加

--- app/interfaces/routers/works.py
+++ app/interfaces/routers/works.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
-from app.infrastructure.inmemory_work_repository import InMemoryWorkRepository
+from app.infrastructure.in_memory.inmemory_work_repository import InMemoryWorkRepository
+from app.usecases.work.create_work.create_work_input_port import CreateWorkInputPort
+from app.usecases.work.create_work.create_work_interactor import CreateWorkInteractor
+from app.usecases.work.create_work.create_work_output_port import CreateWorkOutputPort
 from app.usecases.work.fetch_work.fetch_work_input_port import FetchWorkInputPort
+from app.usecases.work.fetch_work.fetch_work_interactor import FetchWorkInteractor
 from app.usecases.work.fetch_work.fetch_work_output_port import FetchWorkOutputPort
-from app.usecases.work.fetch_work.fetch_work_usecase import FetchWorkUsecase
-from app.usecases.work.fetch_works.fetch_works_usecase import FetchWorksUsecase
+from app.usecases.work.fetch_works.fetch_works_interactor import FetchWorksInteractor
+from app.usecases.work.fetch_works.fetch_works_output_port import FetchWorksOutputPort
 
 router = APIRouter(
     prefix="/works",
@@ -13,16 +19,36 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def get_works():
-    usecase = FetchWorksUsecase(repository=InMemoryWorkRepository())
-    response = usecase.handle()
-    return response
+@router.get("/", response_model=FetchWorksOutputPort)
+def get_works() -> JSONResponse:
+    usecase = FetchWorksInteractor(repository=InMemoryWorkRepository())
+    output = usecase.handle()
+    return JSONResponse(
+        content=jsonable_encoder(output),
+    )
 
 
-@router.get("/{work_id}")
-def get_work(work_id: str) -> FetchWorkOutputPort:
-    input = FetchWorkInputPort(work_id=work_id)
-    usecase = FetchWorkUsecase(repository=InMemoryWorkRepository())
-    response = usecase.handle(input)
-    return FetchWorkOutputPort(id=response.id, title=response.title)
+@router.get("/{work_id}", response_model=FetchWorkOutputPort)
+def get_work(
+    params: FetchWorkInputPort = Depends(FetchWorkInputPort),
+) -> JSONResponse:
+    usecase = FetchWorkInteractor(repository=InMemoryWorkRepository())
+    output = usecase.handle(params)
+    return JSONResponse(
+        content=jsonable_encoder(output),
+    )
+
+
+@router.post(
+    "/", response_model=CreateWorkOutputPort, status_code=status.HTTP_201_CREATED
+)
+def create_work(
+    work: CreateWorkInputPort,
+) -> JSONResponse:
+    usecase = CreateWorkInteractor(repository=InMemoryWorkRepository())
+    output = usecase.handle(work)
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=jsonable_encoder(output),
+        headers={"Location": f"/works/{output.work.id}"},
+    )
